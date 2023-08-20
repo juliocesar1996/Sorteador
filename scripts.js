72e4081bbc5fdf63814b4a37ded353c09937ebdf
juliocesar1996 <juliocesardesouzaribeiro123456@gmail.com>
2023-08-20 17:21:35 -0300

    Projeto finalizado

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ function generateNumber() {
     const min = Math.floor(document.querySelector(".min").value)
     const max = Math.ceil(document.querySelector(".max").value)
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert(result)
+    if (min < max) {
+        alert(result)
+    }else{
+        alert("o valor minimo tem que ser menor que o valor máximo!")
+    }
 }
 myButton.addEventListener("click", generateNumber)
